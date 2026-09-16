ed9dcb7c836898e1ecb25bf25b74d953ad3c7dd1
Thomas Worth <tworth77@cmc.edu>
2026-09-16 15:40:11 -0700

    fixed simple python

--- src/simple.py
+++ src/simple.py
@@ -20,7 +20,7 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-
+    return list(filter(lambda x: x % 2 == 0, range(n + 1)))
 
 def threes(n):
     '''
@@ -37,7 +37,7 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-
+    return list(filter(lambda x: '3' in str(x), range(n + 1)))
 
 def small_words(text):
     '''
@@ -55,7 +55,7 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-
+    return list(filter(lambda w: len(w) < 5, text.split()))
 
 def squares(n):
     '''
@@ -71,7 +71,7 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-
+    return list(map(lambda x: x * x, range(1, n + 1)))
 
 def lengths(strings):
     '''
@@ -84,3 +84,4 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    return list(map(len, strings))
