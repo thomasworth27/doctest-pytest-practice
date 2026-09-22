01a84a15e3b630da2d30af1231105d7ac9560b8b
Thomas Worth <tworth77@cmc.edu>
2026-09-22 10:59:20 -0700

    bracket errors

--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -47,10 +47,10 @@ def test_link_with_italic_text():
 
 def test_every_feature_on_one_line():
     line = 'See the [docs](https://x.com) for **more** _info_.'
-    expected = (
+    expected =
         'See the <a href="https://x.com">docs</a> '
         'for <b>more</b> <i>info</i>.'
-        )
+
     assert compile_all(line) == expected
 
 
