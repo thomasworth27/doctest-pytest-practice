67fd315a109101298c32ea35e55f5dedbba37e1e
Thomas Worth <tworth77@cmc.edu>
2026-09-22 20:34:08 -0700

    fixed brackets again

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -43,7 +43,7 @@ def test_small_words_of_squares():
     assert small_words(text) == [
         '1', '4', '9', '16', '25', '36',
         '49', '64', '81', '100', '121', '144',
-        ]
+    ]
 
 
 def test_lengths_of_squares_as_strings():
